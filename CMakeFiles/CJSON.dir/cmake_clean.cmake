e70cbf9017108f8d806f3f7dc2f92bd4611c691d
ThomasBaumkircher <thomasbaumkircher10@gmail.com>
2023-11-29 22:04:38 +0100

    removed cmake and addec custom makefile

--- CMakeFiles/CJSON.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CJSON.dir/src/Parser.c.o"
-  "CMakeFiles/CJSON.dir/src/Parser.c.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/CJSON.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
